0890407ea424824cd7e79f3c7d74f1903aaeda6b
Luis <lfponton@gmail.com>
2022-04-20 07:28:01 +0200

    implemented tested binary tree node

--- src/BinaryTreeNode.java
+++ src/BinaryTreeNode.java
@@ -1,3 +1,44 @@
-public class BinaryTreeNode
+public class BinaryTreeNode<E>
 {
+  private E element;
+  private BinaryTreeNode<E> leftChild;
+  private BinaryTreeNode<E> rightChild;
+
+  public BinaryTreeNode(E element)
+  {
+    if (element == null) {
+      throw new IllegalArgumentException("The node cannot have a null element");
+    }
+    this.element = element;
+  }
+
+  public E getElement()
+  {
+    return element;
+  }
+
+  public void setElement(E element)
+  {
+    this.element = element;
+  }
+
+  public BinaryTreeNode<E> getLeftChild()
+  {
+    return leftChild;
+  }
+
+  public void addLeftChild(BinaryTreeNode<E> leftChild)
+  {
+    this.leftChild = leftChild;
+  }
+
+  public BinaryTreeNode<E> getRightChild()
+  {
+    return rightChild;
+  }
+
+  public void addRightChild(BinaryTreeNode<E> rightChild)
+  {
+    this.rightChild = rightChild;
+  }
 }
--- test/BinaryTreeNodeTest.java
+++ test/BinaryTreeNodeTest.java
@@ -1,3 +1,49 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BinaryTreeNodeTest
 {
+  @Test
+  public void CreatingNodeWithNullElementThrowsIllegalArgumentException() {
+    assertThrows(IllegalArgumentException.class,
+        () -> new BinaryTreeNode<Integer>(null));
+  }
+
+  @Test
+  public void CreatingNodeWithElementAddsElementToNode()
+  {
+    Integer element = 1;
+    BinaryTreeNode<Integer> node = new BinaryTreeNode<>(element);
+    assertEquals(element, node.getElement());
+  }
+
+  @Test
+  public void SettingElementAddsTheElementToTheNode()
+  {
+    Integer element = 10;
+    BinaryTreeNode<Integer> node = new BinaryTreeNode<>(element);
+    Integer newElement = 50;
+    node.setElement(newElement);
+    assertEquals(newElement, node.getElement());
+  }
+
+  @Test
+  public void AddingLeftChildAddsLeftChildToNode() {
+    Integer element = 24;
+    BinaryTreeNode<Integer> node = new BinaryTreeNode<>(element);
+    Integer leftElement = 45;
+    BinaryTreeNode<Integer> leftChild = new BinaryTreeNode<>(leftElement);
+    node.addLeftChild(leftChild);
+    assertEquals(leftChild.getElement(), node.getLeftChild().getElement());
+  }
+
+  @Test
+  public void AddingRightChildAddsRightChildToNode() {
+    Integer element = 80;
+    BinaryTreeNode<Integer> node = new BinaryTreeNode<>(element);
+    Integer rightElement = 32;
+    BinaryTreeNode<Integer> rightChild = new BinaryTreeNode<>(rightElement);
+    node.addRightChild(rightChild);
+    assertEquals(rightChild.getElement(), node.getRightChild().getElement());
+  }
 }
