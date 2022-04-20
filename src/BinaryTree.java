1258999d1b3fec66f2408f6f4848f1134db917d9
Luis <lfponton@gmail.com>
2022-04-20 08:08:30 +0200

    partially implemented and tested binary tree

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,3 +1,31 @@
-public class BinaryTree
+public class BinaryTree<E>
 {
+  BinaryTreeNode<E> root;
+  int size;
+
+  public BinaryTree() {
+
+  }
+
+  public void setRoot(BinaryTreeNode<E> root) {
+    if (root.getLeftChild() == null && root.getRightChild() == null)
+    {
+      this.root = root;
+      size++;
+    }
+    else
+      throw new IllegalArgumentException("Cannot set a root with children");
+  }
+
+  public BinaryTreeNode<E> getRoot() {
+    return root;
+  }
+
+  public int size() {
+    return size;
+  }
+
+  public boolean isEmpty() {
+    return size == 0;
+  }
 }
--- test/BinaryTreeTest.java
+++ test/BinaryTreeTest.java
@@ -1,3 +1,39 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 public class BinaryTreeTest
 {
+  private BinaryTree<Integer> tree;
+
+  @BeforeEach
+  public void setUp() {
+    tree = new BinaryTree<>();
+  }
+
+  @Test
+  public void CreatingTreeSetsRootToNullAndSizeToZeroAndEmpty()
+  {
+    assertNull(tree.getRoot());
+    assertEquals(0, tree.size());
+    assertTrue(tree.isEmpty());
+  }
+
+  @Test
+  public void SettingRootAddsTheRootToTheTreeAndIncreasesSize() {
+    Integer element = 78;
+    BinaryTreeNode<Integer> node = new BinaryTreeNode<>(element);
+    tree.setRoot(node);
+    assertEquals(element, tree.getRoot().getElement());
+    assertEquals(1, tree.size());
+  }
+
+  @Test
+  public void SettingRootWithChildrenThrowIllegalArgumentException() {
+    Integer element = 89;
+    BinaryTreeNode<Integer> node = new BinaryTreeNode<>(element);
+    BinaryTreeNode<Integer> leftChild = new BinaryTreeNode<>(element);
+    node.addLeftChild(leftChild);
+    assertThrows(IllegalArgumentException.class,
+        ()-> tree.setRoot(node));
+  }
 }
